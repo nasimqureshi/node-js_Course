5fdd432c331505afc37dbb9deb0693b35f9993ca
nasimqureshi <qureshi.nasim@yahoo.com>
2022-12-10 21:02:44 -0600

    my commit

--- global.js
+++ global.js
@@ -1,5 +1,11 @@
-function Hello(name)
-{
-    console.log('Hello'+ name);
-}
-Hello(' Nasim');
+// function Hello(name)
+// {
+//     console.log('Hello'+ name);
+// }
+// Hello(' Nasim');
+
+
+console.log(global);
+globalThis.setTimeout(() => {
+    console.log('in the time out')
+}, 3000);
