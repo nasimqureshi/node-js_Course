788742908eb33f66df3cc73e2efe8dce19fd819f
nasimqureshi <qureshi.nasim@yahoo.com>
2022-12-11 18:37:42 -0600

    commit

--- server.js
+++ server.js
@@ -4,9 +4,11 @@ const server = http.createServer((req, res) => {
     console.log(req.url, req.method);
 
     // set header content type
-res.setHeader('Content-type', 'text/plain');
+res.setHeader('Content-type', 'text/html');
 
-res.write('Hello. Nasim how are you');
+res.write('<head><link rel="stylesheet" href="#"</head>');
+res.write('<p>I am ok</p>');
+res.write('<p>How about you</p>');
 res.end();
 });
 
