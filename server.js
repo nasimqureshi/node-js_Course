058fd04b9c8f9f9d0302d79f5105889a922b35ac
nasimqureshi <qureshi.nasim@yahoo.com>
2022-12-11 18:28:59 -0600

    commit

--- server.js
+++ server.js
@@ -2,7 +2,14 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
     console.log(req.url, req.method);
+
+    // set header content type
+res.setHeader('Content-type', 'text/plain');
+
+res.write('Hello. Nasim how are you');
+res.end();
 });
+
 
 server.listen(3000, 'localhost',  () => {
     console.log('listening for requests on post 3000 for Nasim Qureshi')
