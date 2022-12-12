7700d2bc4e637537b9253bdb56c930b0c3243ecc
nasimqureshi <qureshi.nasim@yahoo.com>
2022-12-11 19:37:27 -0600

    commit

--- server.js
+++ server.js
@@ -1,15 +1,23 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
     console.log(req.url, req.method);
 
     // set header content type
 res.setHeader('Content-type', 'text/html');
+// send an html file
+fs.readFile('./views/index.html', (err, data) => {
+    if(err){
+        console.log(err);
+        res.end();
+    }else {
+        res.write(data);
+        res.end();
+    }
+});
 
-res.write('<head><link rel="stylesheet" href="#"</head>');
-res.write('<p>I am ok</p>');
-res.write('<p>How about you</p>');
-res.end();
+
 });
 
 
